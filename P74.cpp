79f13cdb5e942f667cc2f31d84f9e576e3d26feb
Finem50 <Finem50@live.com>
2018-09-10 20:53:41 -0500

    Exercise with the scope of a variable in a block

---
+++ P74.cpp
@@ -0,0 +1,20 @@
+//This program illustrates the scope of variables in a block
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int x = 10;
+
+  for(int i=0; i<x; i++){
+
+    x = 5;
+    cout << "i = " << i << endl;
+  }
+
+  cout << "x = " << x << endl;
+
+  return 0;
+}
